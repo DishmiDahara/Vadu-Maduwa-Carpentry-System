b5fad52d630dd8c7f2320a4d962398563548e7af
DaharaGajanayaka <daharagajanayaka@gmail.com>
2026-07-29 01:37:07 +0530

    Add vercel.json and set relative base path for Vercel deployment

--- woodcraftpro-frontend/vite.config.js
+++ woodcraftpro-frontend/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Vadu-Maduwa-Carpentry-System/',
+  base: './',
   server: {
     host: true,
     port: 5173
   }
 })
+
 
 
